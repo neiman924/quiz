584c7f13df63d21795067c3d4fc9ec3cc14cf795
Hossein Neiman <neiman924@gmail.com>
2022-04-26 18:31:24 -0700

    update 4-26 06:30 pm

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,5 +1,9 @@
 
 var qResult = document.querySelector("#result");
+// var wrongAnswer = document.querySelector("#wrongAnswers");
+// var quizRepeat = document.querySelector("#quizRepeat");
+
+
 //-------------------------------------------------------------------------------
 function result(){
     var result = localStorage.getItem("quizAnswers");
@@ -7,21 +11,21 @@ function result(){
     var quizListlength = parseInt(localStorage.getItem('qLength'));
     quizListlength = quizListlength - result;
     if(result!=undefined){
-            var r1 = document.createElement("div");//check answer 
-            var r2 = document.createElement("div");//check answer 
-            var r3 = document.createElement("div");//check answer 
+            var r1 = document.createElement("h2");//check answer 
+            var r2 = document.createElement("h2");//check answer 
+            var r3 = document.createElement("h2");//check answer 
 
-            r1.textContent = result;
-            r2.textContent = qRepeat;
-            r3.textContent = quizListlength;
+            r1.textContent = "Correct answers : " + result + "   "  + "Quiz Repeat : " + qRepeat + "   " + "Wrong Answers : " + quizListlength;
+            // r2.textContent = qRepeat;
+            // r3.textContent = quizListlength;
 
             r1.setAttribute("class" , "check");
-            r2.setAttribute("class" , "check");
-            r3.setAttribute("class" , "check");
+            // r2.setAttribute("class" , "check");
+            // r3.setAttribute("class" , "check");
 
             qResult.appendChild(r1);
-            qResult.appendChild(r2);
-            qResult.appendChild(r3);
+            // wrongAnswer.appendChild(r2);
+            // qResult.appendChild(r1);
         }
     }
   //-------------------------------------------------------------------------------
